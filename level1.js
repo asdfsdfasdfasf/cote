89fe46f52a9e299f7fa0a16d53d7edbf06909ffc
HwangSoyeon <hsy150701@naver.com>
2026-04-19 17:26:47 +0900

    Create level1.js

---
+++ level1.js
@@ -0,0 +1,19 @@
+function solution(numbers) {
+    var answer = [];
+    
+    for (var i = 0; i < numbers.length; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            var sum = numbers[i] + numbers[j];
+            
+            if (!answer.includes(sum)) {
+                answer.push(sum);
+            }
+        }
+    }
+    
+    answer.sort(function(a, b) {
+        return a - b;
+    });
+    
+    return answer;
+}

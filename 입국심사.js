ea756ed31e1e3a3a522588d841282343c2e180fc
HwangSoyeon <hsy150701@naver.com>
2026-04-28 16:18:19 +0900

    Create 입국심사.js

---
+++ 입국심사.js
@@ -0,0 +1,28 @@
+function solution(n, times) {
+    times.sort((a, b) => a - b);
+
+    let left = 1;
+    let right = times[0] * n;
+    let answer = right;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        let count = 0;
+
+        for (let i = 0; i < times.length; i++) {
+            count += Math.floor(mid / times[i]);
+
+            if (count >= n) break;
+        }
+
+        if (count >= n) {
+            answer = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return answer;
+}
